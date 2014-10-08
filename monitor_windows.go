944c4663e1de6be60dcbb8b92a2126142784586c
andö <lifeandcoding@gmail.com>
2014-10-08 08:20:55 +0300

    Fix build errors in windows.

--- monitor_windows.go
+++ monitor_windows.go
@@ -1,13 +1,6 @@
 package panicwrap
 
-import (
-	"github.com/mitchellh/osext"
-	"io"
-	"os"
-	"os/exec"
-	"os/signal"
-	"syscall"
-)
+import "fmt"
 
 func monitor(c *WrapConfig) (int, error) {
 	return -1, fmt.Errorf("Monitor is not supported on windows")
